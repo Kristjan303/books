414b11899e05cc11e8e05e7f2e8b3b93c197538e
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-10-18 13:03:51 +0300

    Add book to localStorage [Delivers #183530866]

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const subbtn = document.getElementById('BTN')
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const isbn = document.getElementById('isbn')
 
 subbtn.addEventListener("click", addbook)
 
 let table = document.getElementById('books')
-const taskList = document.querySelector('ul')
 const bookDelete = document.querySelector('table')
 
 bookDelete.addEventListener('click', deleteBook)
@@ -14,15 +16,19 @@ function addbook(e) {
     let cell2 = row.insertCell(1)
     let cell3 = row.insertCell(2)
     let cell4 = row.insertCell(3)
-    cell1.innerHTML = document.getElementById('title').value;
-    cell2.innerHTML = document.getElementById('author').value;
-    cell3.innerHTML = document.getElementById('isbn').value;
+    cell1.innerHTML = title.value;
+    cell2.innerHTML = author.value;
+    cell3.innerHTML = isbn.value;
 
     const a = document.createElement('a')
     a.appendChild(document.createTextNode('X'))
     a.className = 'blue-text text-darken-2 secondary-content'
     a.setAttribute('href', '#')
     cell4.appendChild(a)
+    addTaskLS(title.value, author.value, isbn.value)
+    title.value = ""
+    author.value = ""
+    isbn.value = ""
 }
 
 function deleteBook(e){
@@ -32,4 +38,16 @@ function deleteBook(e){
 
         }
     }
+}
+
+function addTaskLS(title, author, isbn) {
+    let books
+    if(localStorage.getItem('books') === null){
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    let book = [title, author, isbn]
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
 }
