6ac2dd871dde88a8416c37c174abc8e25e490651
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-10-19 13:32:47 +0300

    Get books from localStorage [Delivers #183568090]

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const subbtn = document.getElementById('BTN')
 const title = document.getElementById('title')
 const author = document.getElementById('author')
 const isbn = document.getElementById('isbn')
-document.addEventListener('DOMContentLoaded', getTasks)
-
+document.addEventListener('DOMContentLoaded', getBooks)
+const masterTable = document.querySelector('#books')
 subbtn.addEventListener("click", addbook)
 
 let table = document.getElementById('books')
@@ -31,25 +31,32 @@ function addbook(e) {
     author.value = ""
     isbn.value = ""
 }
-function getTasks(){
-    let Book
+function getBooks(){
+    let books
     if(localStorage.getItem('books') === null){
         books = []
     } else {
         books = JSON.parse(localStorage.getItem('books'))
     }
-    books.forEach((task) => {
-        const tr = document.createElement('th')
-        tr.appendChild(document.createTextNode(task))
-        tr.className = 'collection-item'
-        const a = document.createElement('a')
-        a.appendChild(document.createTextNode('X'))
-        a.className = 'blue-text text-darken-2 secondary-content'
-        a.setAttribute('href', '#')
-        tr.appendChild(a)
-        // add to list
-        const th = document.querySelector('ul')
-        th.appendChild(tr)
+    books.forEach((book) => {
+        console.log(book)
+
+        const getRow = masterTable.insertRow()
+
+        const getCell1 = getRow.insertCell()
+        const getCell2 = getRow.insertCell()
+        const getCell3 = getRow.insertCell()
+        const getCell4 = getRow.insertCell()
+
+        const getCross = document.createElement('a')
+        getCross.appendChild(document.createTextNode('X'))
+        getCross.className = 'red-text text-darken-2 secondary-content'
+        getCross.setAttribute('href', '#')
+
+        getCell1.innerHTML = book[0].toString()
+        getCell2.innerHTML = book[1].toString()
+        getCell3.innerHTML = book[2].toString()
+        getCell4.appendChild(getCross)
     })
 }
 function deleteBook(e){
